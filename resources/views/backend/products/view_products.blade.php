74add52ecd6f68c2d18c79d58edb453d9264322c
BlueVortex123 <duicu.andrei@gmail.com>
2021-08-17 16:52:08 +0300

    fix Contracts and Products Index layout

--- resources/views/backend/products/view_products.blade.php
+++ resources/views/backend/products/view_products.blade.php
@@ -1,44 +1,45 @@
 @extends('layout')
 
 @section('content')
-<div class="content-wrapper">
-    <div class="container-full">
-        <div class="row">
-            <div class="col-lg-12 margin-tb">
-                <div class="pull-right"><h4>Task 2</h4></div>
-            </div>
-            <div class="pull-right">
-                <a href="{{ route('add.contracts') }}" class="btn btn-success">Store new Products</a>
-            </div>
-        </div>
 
-        <div class="row">
-            <table class="table table-bordered">
-                <thead>
-                    <tr>
-                        <th>No</th>
-                        <th>Name</th>
-                        <th>Contract</th>
-                        <th>Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($products as $key => $product )
-                    <tr>
-                        <td>{{ $key+1 }}</td>
-                        <td>{{ $product->name }}</td>
-                        <td>{{ $product['contracts']['name'] }}</td>
-                        <td>
-                            <a href="#" class="btn btn-info">Edit</a>
-                            <a href="#" class="btn btn-danger">Delete</a>
-                        </td>
-                    </tr>
-                    @endforeach
-                </tbody>
-            </table>
+<div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header">
+                    <h1>Products</h1>
+                    <div class="float-right">
+                        <a href="{{ route('add.products') }}" class="btn btn-success">Store new Product</a>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <div class="table-responsive">
+                                <table class="table table-bordered">
+                            <thead>
+                                <tr>
+                                    <th>No</th>
+                                    <th>Name</th>
+                                    <th>Contract</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach($products as $key => $product )
+                                <tr>
+                                    <td>{{ $key+1 }}</td>
+                                    <td>{{ $provider->name }}</td>
+                                    <td>{{ $provider['contracts']['name'] }}</td>
+                                    <td>
+                                        <a href="" class="btn btn-info">Edit</a>
+                                        <a href="" class="btn btn-danger">Delete</a>
+                                    </td>
+                                </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
-</div>
-
-        
+            
 @endsection()
